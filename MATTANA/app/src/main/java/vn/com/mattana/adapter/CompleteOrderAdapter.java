de7a0f320b3dcd05f718e65b21e9178ec17d57cb
Le Hoai Nam <lenam.leo@gmail.com>
2018-02-26 14:18:47 +0700

    nam

---
+++ MATTANA/app/src/main/java/vn/com/mattana/adapter/CompleteOrderAdapter.java
@@ -0,0 +1,101 @@
+package vn.com.mattana.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+import vn.com.mattana.dms.R;
+import vn.com.mattana.dms.order.CompleteOrderActivity;
+import vn.com.mattana.model.api.order.ProductInfo;
+import vn.com.mattana.util.MRes;
+
+/**
+ * Created by HAI on 2/26/2018.
+ */
+
+public class CompleteOrderAdapter extends RecyclerView.Adapter<CompleteOrderAdapter.MyViewHolder> {
+
+    private List<ProductInfo> productOrders;
+    private CompleteOrderActivity activity;
+
+
+    public CompleteOrderAdapter(CompleteOrderActivity activity) {
+        productOrders = MRes.getInstance().getProductOrder();
+        this.activity = activity;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.complete_order_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+
+        final ProductInfo order = productOrders.get(position);
+
+
+        holder.name.setText(order.getName());
+        holder.code.setText("Mã SP: " + order.getCode());
+        holder.detail.setText("SL đặt: " + order.getQuantityBuy());
+        double price = order.getPrice() * order.getQuantityBuy();
+
+        holder.price.setText(MRes.getInstance().formatMoneyToText(price));
+
+        holder.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MRes.getInstance().removeProductOrderAt(position);
+                activity.notifyAdapter();
+                activity.resetMoneyAll();
+            }
+        });
+
+        holder.detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.changeQuantity( position);
+            }
+        });
+
+        holder.imgEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.changeQuantity( position);
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return productOrders.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, code, detail, price;
+        public ImageView imgDelete, imgEdit;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.name);
+            code = (TextView) view.findViewById(R.id.code);
+            detail = (TextView) view.findViewById(R.id.detail);
+            imgDelete = (ImageView) view.findViewById(R.id.imgdelete);
+            price = (TextView) view.findViewById(R.id.price);
+            imgEdit  = (ImageView) view.findViewById(R.id.imgedit);
+        }
+    }
+
+}
--- MATTANA/app/src/main/java/vn/com/mattana/dms/order/CompleteOrderActivity.java
+++ MATTANA/app/src/main/java/vn/com/mattana/dms/order/CompleteOrderActivity.java
@@ -1,13 +1,36 @@
 package vn.com.mattana.dms.order;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import vn.com.mattana.adapter.CompleteOrderAdapter;
 import vn.com.mattana.dms.BaseActivity;
 import vn.com.mattana.dms.R;
+import vn.com.mattana.model.api.order.ProductInfo;
+import vn.com.mattana.util.MRes;
+import vn.com.mattana.view.DividerItemDecoration;
 
 public class CompleteOrderActivity extends BaseActivity {
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+    CompleteOrderAdapter adapter;
+
+    @BindView(R.id.txtmoney)
+    TextView txtMoney;
+
+    @BindView(R.id.agency)
+    TextView agency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +38,80 @@ public class CompleteOrderActivity extends BaseActivity {
         setContentView(R.layout.activity_complete_order);
         ButterKnife.bind(this);
         createToolbar();
+
+        adapter = new CompleteOrderAdapter(this);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        resetMoneyAll();
+
+        agency.setText(MRes.getInstance().agency.getStore() +" - " + MRes.getInstance().agency.getCode());
     }
 
 
     public void orderClick(View view) {
 
     }
+
+
+    public void notifyAdapter() {
+        adapter.notifyDataSetChanged();
+    }
+
+
+    public void resetMoneyAll() {
+        double price = 0;
+
+        for(ProductInfo order: MRes.getInstance().getProductOrder()) {
+            price+= order.getPrice() * order.getQuantityBuy();
+        }
+        txtMoney.setText(MRes.getInstance().formatMoneyToText(price));
+
+    }
+
+    public void changeQuantity(final int position) {
+
+        final ProductInfo info = MRes.getInstance().getProductOrder().get(position);
+
+        View viewDialog = CompleteOrderActivity.this.getLayoutInflater().inflate(R.layout.dialog_change_quantity_order, null);
+        final EditText eQantity = (EditText) viewDialog.findViewById(R.id.euantity);
+        eQantity.setText("" + info.getQuantityBuy());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Thao tác");
+        builder.setMessage("Thay đổi số lượng mua");
+        builder.setIcon(R.mipmap.ic_logo);
+        builder.setView(viewDialog);
+        builder.setNegativeButton("Thôi", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        builder.setPositiveButton("Nhập", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (!TextUtils.isEmpty(eQantity.getText().toString())) {
+                    try{
+                        int quantity  = Integer.parseInt(eQantity.getText().toString());
+                        MRes.getInstance().getProductOrder().get(position).setQuantityBuy(quantity);
+                        adapter.notifyDataSetChanged();
+                    }catch (Exception e) {
+
+                    }
+                }else {
+                    commons.makeToast(CompleteOrderActivity.this, "Nhập số lượng").show();
+                }
+
+            }
+        });
+
+        builder.show();
+    }
+
 }
